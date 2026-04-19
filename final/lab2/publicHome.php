483128b622241d888b1987e56dedf7396d62e410
SohihWahid <140302133+SohihWahid@users.noreply.github.com>
2026-04-19 14:56:42 +0600

    Add files via upload

---
+++ final/lab2/publicHome.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PUBLIC HOME</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <form action="" method="post">
+        <fieldset>
+            <table class="custom-table">
+                <tr>
+                    <td>
+                        <div class="header-row">
+                            <div class="company-name">Company</div>
+                            <div class="header-links">
+                                <a href="Home">Home</a>
+                                <a href="Login">Login</a>
+                                <a href="registration.php">Registration</a>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+                <tr><td><h3>Welcome to Our Company</h3></td></tr>
+                <tr><td><h5>copyright © 2017</h5></td></tr>
+            </table>
+        </fieldset>
+    </form>
+</body>
+</html>
---
+++ final/lab2/registration.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration</title>
+</head>
+<body>
+    <table class="custom-table">
+                <tr>
+                    <td>
+                        <div class="header-row">
+                            <div class="company-name">Company</div>
+                            <div class="header-links">
+                                <a href="Home">Home</a>
+                                <a href="Login">Login</a>
+                                <a href="Registration">Registration</a>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+                <tr><td><div>
+                    <form action="">
+                        <fieldset>
+                            <legend>Registration</legend>
+                            <label for="name">Name:</label>
+                            <input type="text" id="name" name="name"><br><br>
+                            <label for="email">Email:</label>
+                            <input type="email" id="email" name="email"><br><br>
+                            <label for="password">Password:</label>
+                            <input type="password" id="password" name="password"><br><br>
+                            <input type="submit" value="Register">
+                        </fieldset>
+                    </form>
+                </div></td></tr>
+                <tr><td><h5></h5></td></tr>
+            </table>
+</body>
+</html>
